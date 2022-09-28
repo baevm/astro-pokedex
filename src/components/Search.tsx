0251596c1e19c375e9430f0de74c069f85902ccc
dezzerlol <misha.6325@gmail.com>
2022-09-28 23:41:26 +0500

    initial

---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='w-full'>
+      <input className='w-full bg-[#515151]' placeholder='Find a pokemon...' />
+    </div>
+  )
+}
+
+export default Search
